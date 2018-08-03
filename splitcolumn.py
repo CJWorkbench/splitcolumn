1fce878f628ef96e9c2899246c9ab6d3fe1257c3
emrig <emrig@users.noreply.github.com>
2018-08-03 18:50:25 -0400

    Updated to handle categories and numbers Added tests

--- splitcolumn.py
+++ splitcolumn.py
@@ -1,19 +1,46 @@
 def render(table, params):
-  delim = params['delimiter']
-  col = params['column']
-  
-  if col=='' or delim=='':
-    return table
-  
-  # the actual split
-  newcols =  table[col].str.split(delim, expand=True)
-  if len(newcols.columns) == 1:
-    return table # we didn't find the delimiter anywhere
-  newcols.columns = [col + ' ' + str(x+1) for x in newcols.columns]
-  
-  # now glue before, split, and after columns together 
-  colloc = table.columns.get_loc(col)
-  start = table.iloc[:, :colloc]
-  end = table.iloc[:, colloc+1:]
-  return pd.concat([start, newcols, end], axis=1)
+    delim = params['delimiter']
+    col = params['column']
+
+    if col == '' or delim == '':
+      return table
+
+    # the actual split, output to str and cat dtypes for now
+    if table[col].dtype.name == 'category':
+        if table[col].cat.categories.dtype == float:
+            newcols = (convert_float(table[col].astype(float), delim)).astype('category')
+        elif table[col].cat.categories.dtype == object:
+            # Must add '' to category if want to remove NaN
+            copy = table[col].copy()
+            if '' not in copy.cat.categories:
+                copy.cat.add_categories([''], inplace=True)
+            newcols = copy.fillna('').str.split(delim, expand=True).astype('category')
+        else:
+            newcols = table[col].astype(str).str.split(delim, expand=True).astype('category')
+    # fill nulls with '' to avoid 'NaN'
+    elif table[col].dtype == float:
+        newcols = convert_float(table[col], delim)
+
+    else:
+        newcols = table[col].astype(str).str.split(delim, expand=True)
+
+    # we didn't find the delimiter anywhere
+    if len(newcols.columns) == 1:
+      return table
+
+    newcols.columns = [col + ' ' + str(x+1) for x in newcols.columns]
+
+    # now glue before, split, and after columns together
+    colloc = table.columns.get_loc(col)
+    start = table.iloc[:, :colloc]
+    end = table.iloc[:, colloc+1:]
+    return pd.concat([start, newcols, end], axis=1)
+
+# Special case: remove decimal if float is a whole number
+def convert_float(col, delim):
+    # Special case: remove decimal if float is a whole number
+    ints = col[col % 1 == 0].astype(int)
+    col = col.fillna('').astype(str)
+    col.update(ints.astype(str))
+    return col.str.split(delim, expand=True)
 
---
+++ test_splitcolumn.py
@@ -0,0 +1,169 @@
+import unittest
+import pandas as pd
+import numpy as np
+from splitcolumn import render
+
+
+class TestSplitColumns(unittest.TestCase):
+
+    def setUp(self):
+        # Test data includes:
+        #  - rows of numeric, string, and categorical types
+        #  - zero entries (which should not be removed)
+        #  - if either column categorical type, retain caegorical
+        self.table = pd.DataFrame([
+            ['a.b',     123,    2.1,    'b.c',      2.1,    '2018-08-03',   123],
+            ['a.b',     123,    2,      'b.c',      2,      '2018-08-03',   123],
+            ['a.b',     123,    2.1,    '',         2.1,    '2018-08-03',   123],
+            ['a.b',     123,    2.1,    'b.c',      2.1,    '2018-08-03',   123],
+            ['a.b',     123,    None,   None,       None,   '2018-08-03',   123]],
+            columns=['stringcol','intcol', 'floatcol','catcol','floatcatcol', 'datecol', 'intcatcol'])
+
+        # Pandas should infer these types anyway, but leave nothing to chance
+        self.table['stringcol'] = self.table['stringcol'].astype(str)
+        self.table['intcol'] = self.table['intcol'].astype(np.int64)
+        self.table['floatcol'] = self.table['floatcol'].astype(np.float64)
+        self.table['catcol'] = self.table['catcol'].astype('category')
+        self.table['floatcatcol'] = self.table['floatcatcol'].astype('category')
+        self.table['datecol'] = self.table['datecol'].astype(str)
+        self.table['intcatcol'] = self.table['intcatcol'].astype('category')
+
+    def construct_expected(self, ref, column, rows):
+        idx = ref.columns.get_loc(column)
+        ref.drop(columns=[column], inplace=True)
+        for offset, col in enumerate(rows):
+            ref.insert(idx + offset, f'{column} {offset + 1}', col)
+        return ref
+
+    def test_NOP(self):
+        params = {'delimiter': '', 'column': 'stringcol'}
+        out = render(self.table, params)
+        self.assertTrue(out.equals(self.table)) # should NOP when first applied
+
+        params = {'delimiter': '-', 'column': ''}
+        out = render(self.table, params)
+        self.assertTrue(out.equals(self.table))
+
+    def test_split_str(self):
+        column = 'stringcol'
+        params = {'delimiter': '.', 'column': column}
+        out = render(self.table, params)
+
+        # Assert old column removed
+        self.assertTrue((set(self.table.columns)-set(out.columns) == set([column])))
+
+        ref = self.construct_expected(self.table, column,
+                   [['a']*5,
+                    ['b']*5])
+
+        pd.testing.assert_frame_equal(out, ref)
+
+    def test_split_int(self):
+        column = 'intcol'
+        params = {'delimiter': '2', 'column': column}
+        out = render(self.table, params)
+        ref = self.table
+
+        # Assert old column removed
+        self.assertTrue((set(self.table.columns) - set(out.columns) == set([column])))
+
+        ref = self.construct_expected(self.table, column,
+                   [['1']*5,
+                    ['3']*5])
+
+        pd.testing.assert_frame_equal(out, ref)
+
+    def test_split_float(self):
+        column = 'floatcol'
+        params = {'delimiter': '.', 'column': column}
+        out = render(self.table, params)
+        ref = self.table
+
+        # Assert old column removed
+        self.assertTrue((set(self.table.columns) - set(out.columns) == set([column])))
+
+        ref = self.construct_expected(self.table, column,
+                   [['2','2','2','2',''],
+                    ['1', None, '1', '1', None]])
+
+        pd.testing.assert_frame_equal(out, ref)
+
+    def test_split_cat(self):
+        column = 'catcol'
+        params = {'delimiter': '.', 'column': column}
+        out = render(self.table, params)
+        ref = self.table
+
+        # Assert old column removed
+        self.assertTrue((set(self.table.columns) - set(out.columns) == set([column])))
+
+        # .replace splits '' into '','nan'
+        ref = self.construct_expected(self.table, column,
+                   [['b','b','','b',''],
+                    ['c','c',np.nan,'c',np.nan]])
+
+        ref['catcol 1'] = ref['catcol 1'].astype('category')
+        ref['catcol 2'] = ref['catcol 2'].astype('category')
+
+        pd.testing.assert_frame_equal(out, ref)
+
+        column = 'intcatcol'
+        params = {'delimiter': '2', 'column': column}
+        out = render(self.table, params)
+        ref = self.table
+
+        # Assert old column removed
+        self.assertTrue((set(self.table.columns) - set(out.columns) == set([column])))
+
+        ref = self.construct_expected(self.table, column,
+                                      [['1'] * 5,
+                                       ['3'] * 5])
+        ref['intcatcol 1'] = ref['intcatcol 1'].astype('category')
+        ref['intcatcol 2'] = ref['intcatcol 2'].astype('category')
+
+        pd.testing.assert_frame_equal(out, ref)
+
+    def test_split_cat_float(self):
+        column = 'floatcatcol'
+        params = {'delimiter': '.', 'column': column}
+        out = render(self.table, params)
+        ref = self.table
+
+        # Assert old column removed
+        self.assertTrue((set(self.table.columns) - set(out.columns) == set([column])))
+
+        ref = self.construct_expected(self.table, column,
+                                      [['2', '2', '2', '2', ''],
+                                       ['1', None, '1', '1', None]])
+
+        ref['floatcatcol 1'] = ref['floatcatcol 1'].astype('category')
+        ref['floatcatcol 2'] = ref['floatcatcol 2'].astype('category')
+
+        pd.testing.assert_frame_equal(out, ref)
+
+    def test_multiple_splits(self):
+        column = 'datecol'
+        params = {'delimiter': '-', 'column': column}
+        out = render(self.table, params)
+        ref = self.table
+
+        # Assert old column removed
+        self.assertTrue((set(self.table.columns) - set(out.columns) == set([column])))
+
+        ref = self.construct_expected(self.table, column,
+                   [['2018']*5,
+                    ['08'] * 5,
+                    ['03'] * 5])
+
+        pd.testing.assert_frame_equal(out, ref)
+
+    def test_no_del_found(self):
+        params = {'delimiter': '!', 'column': 'stringcol'}
+        out = render(self.table, params)
+        ref = self.table
+        self.assertTrue(out.equals(ref))
+
+if __name__ == '__main__':
+    unittest.main()
+
+
