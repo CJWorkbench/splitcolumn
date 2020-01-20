3e1d1d772718360813af0d451dff2f47b1335a1c
Adam Hooper <adam@adamhooper.com>
2020-01-20 10:39:04 -0500

    Use gen_unique_clean_colnames.
    TODO warn about invalid colnames. (I didn't implement this because I'm
    thinking we should probably translate those warnings within cjwmodule
    itself.)
    
    I don't think gen_unique_clean_colnames() is the most-intuitive choice,
    in a vacuum. Splitting [A, A 2] by A to produce 3 columns will create
    [[A 1, A 4, A 3], A 2] -- the "A 4" is confusing and users might expect
    "A 2 2". But there's no right answer here, and a warning should be good
    enough. (I think "warning" in Workbench means, "Fix me!")
    
    Should fix error on production.

--- splitcolumn.py
+++ splitcolumn.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import re
+from cjwmodule.util.colnames import gen_unique_clean_colnames
 
 
 MaxNResultColumns = 100  # prevent out-of-memory
@@ -12,7 +13,7 @@ def _migrate_params_v0_to_v1(params):
 
     v1: 'method' is an index into "Delimiter|Chars from Left|Chars from Right"
     """
-    return {**params, 'method': 0, 'numchars': 1}
+    return {**params, "method": 0, "numchars": 1}
 
 
 def _migrate_params_v1_to_v2(params):
@@ -21,14 +22,14 @@ def _migrate_params_v1_to_v2(params):
 
     v2: 'method' is one of 'delimiter', 'left', 'right'
     """
-    return {**params, 'method': ['delimiter','left','right'][params['method']]}
+    return {**params, "method": ["delimiter", "left", "right"][params["method"]]}
 
 
 def migrate_params(params):
-    if 'method' not in params:
+    if "method" not in params:
         params = _migrate_params_v0_to_v1(params)
 
-    if isinstance(params['method'], int):
+    if isinstance(params["method"], int):
         params = _migrate_params_v1_to_v2(params)
 
     return params
@@ -36,18 +37,17 @@ def migrate_params(params):
 
 # Take a string column, split according to user's chosen method
 # Returns a table (multiple columns) of string category type, or None meaning NOP
-def dosplit(coldata, *, method: str, delimiter: str, numchars: int):
-    if method == 'delimiter':
+def split_series(coldata, *, method: str, delimiter: str, numchars: int):
+    if method == "delimiter":
         # pandas does not split by string (despite what its docs say). It
         # splits by regex. Turn our string into a regex so we can split it.
-        return coldata.str.split(re.escape(delimiter), expand=True,
-                                 n=MaxNResultColumns)
+        return coldata.str.split(re.escape(delimiter), expand=True, n=MaxNResultColumns)
     else:
         # otherwise, split off left or right chars
         if numchars <= 0:
-            return 'Please choose a positive number of characters.'
+            return "Please choose a positive number of characters."
 
-        if method == 'left':
+        if method == "left":
             return pd.concat([coldata.str[:numchars], coldata.str[numchars:]], axis=1)
         else:
             # 'Characters from right
@@ -55,15 +55,15 @@ def dosplit(coldata, *, method: str, delimiter: str, numchars: int):
 
 
 def render(table, params):
-    colname = params.pop('column')
+    colname = params.pop("column")
 
-    if colname == '':
+    if colname == "":
         return table
 
-    if params['method'] == 'delimiter' and not params['delimiter']:
-        return table   # Empty delimiter, NOP
+    if params["method"] == "delimiter" and not params["delimiter"]:
+        return table  # Empty delimiter, NOP
 
-    newcols = dosplit(table[colname], **params)
+    newcols = split_series(table[colname], **params)
 
     # NOP if input is bad or we didn't find the delimiter anywhere
     if isinstance(newcols, str):
@@ -74,19 +74,25 @@ def render(table, params):
         return table
 
     # preserve category-ness
-    if hasattr(table[colname], 'cat'):
-        newcols = newcols.astype('category')
+    if hasattr(table[colname], "cat"):
+        newcols = newcols.astype("category")
 
-    # Number the split columns
-    newcols.columns = [colname + ' ' + str(x+1)
-                       for x in range(len(newcols.columns))]
+    # Name the new columns
+    conflict_colnames = list(table.columns)
+    conflict_colnames.remove(colname)
+    uccolnames = gen_unique_clean_colnames(
+        ["%s %d" % (colname, i + 1) for i in range(len(newcols.columns))],
+        existing_names=conflict_colnames,
+    )
+    newcols.columns = [ucc.name for ucc in uccolnames]
+    # TODO warn about renames
 
     if len(table.columns) > 1:
         # glue before, split, and after columns together
         # TODO test this
         colloc = table.columns.get_loc(colname)
         start = table.iloc[:, :colloc]
-        end = table.iloc[:, colloc+1:]
+        end = table.iloc[:, colloc + 1 :]
         return pd.concat([start, newcols, end], axis=1)
     else:
         return newcols
